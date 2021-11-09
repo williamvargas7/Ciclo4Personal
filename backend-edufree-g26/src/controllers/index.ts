d7335e23c2ee66e74895e80c1bb99d890959264e
williamvargas7 <86165574+williamvargas7@users.noreply.github.com>
2021-11-09 15:43:42 -0500

    Creacion de modelos y relacion

--- backend-edufree-g26/src/controllers/index.ts
+++ backend-edufree-g26/src/controllers/index.ts
@@ -1,3 +1,5 @@
 export * from './ping.controller';
 export * from './programa-academico-asignatura.controller';
 export * from './asignatura-programa-academico.controller';
+export * from './programa-academico.controller';
+export * from './asignatura.controller';
